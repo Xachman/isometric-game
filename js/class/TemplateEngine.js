2cbf4af4aa8ea1c11e9c012af2ce73b774d89406
xach <xach@xach-p7-1205>
2016-06-05 21:31:47 -0400

    added template looping

--- js/class/TemplateEngine.js
+++ js/class/TemplateEngine.js
@@ -1,61 +1,68 @@
-TemplateEngine = function() {
+TemplateEngine = function () {
     this.ajax = new Ajax();
     this.parser = new DOMParser();
-    this.regex = /{{([^%>]+)?}}/g;
-    this.getTemplate = function(url, data, callback) {
+    this.regex = /{{\s*([^}]+)\s*}}/g;
+    this.getTemplate = function (url, data, callback) {
         url = this.randomizeUrl(url);
         var obj = this;
-        this.ajax.get(url, function(view) {
+        this.ajax.get(url, function (view) {
             obj.insertData(view, data);
         });
     }
-    this.randomizeUrl = function(url) {
-        return url+'?decache='+Date.now();
+    this.randomizeUrl = function (url) {
+        return url + '?decache=' + Date.now();
     }
-    
-    this.insertData = function(view, data) {
+
+    this.insertData = function (view, data) {
         this.findLoops(view, data);
-        
+
     }
-    
-    this.findLoops = function(view, data) {
-        
+
+    this.findLoops = function (view, data) {
+
         var doc = this.parser.parseFromString(view, "text/html");
         var loops = doc.querySelectorAll("[data-loop]");
-        
+
         //console.log(loops);
         this.processLoop(loops, data);
     }
-    
-    this.processLoop = function(loops, data) {
+
+    this.processLoop = function (loops, data) {
         var obj = this;
-        
-        this.loop(loops, function(loop, item){
-            var html = obj.replaceLoop(loop.outerHTML, data);
-            console.log(data);
+
+        this.loop(loops, function (loop, item) {
+            var directive = loop.getAttribute('data-loop');
+            var html = obj.replaceLoop(loop, data, directive);
             console.log(html);
-            var directive = match
         });
     }
-    this.loop = function(items, callback) {
-        for(var i = 0; i < items.length; i++) {
+    this.loop = function (items, callback) {
+        for (var i = 0; i < items.length; i++) {
             callback(items[i], i);
         }
     }
-    
-    this.replaceLoop = function(html, data, callback) {
-        var count = 0;
-        var directive;
-        while(match = this.regex.exec(html)) {
-            count++;
-            if(count = 1){
-                directive = match[1];
-            }else{
-                html = html.replace(match[0], data[match[1]]);
-            }
-            
-        }
-        return html;
+
+    this.replaceLoop = function (loop, data, directive) {
+        var template = loop.innerHTML
+        var newHTML = '';
+        var obj = this;
+        directive = directive.split(' ');
+        var match = obj.regex.exec(template)
+        var dataProperty = directive[0];
+        this.loop(data[dataProperty], function (item, index) {
+            var properties = match[1].split('.');
+            properties.shift();
+            var insert = item;
+            obj.loop(properties, function (property, index) {
+                insert = insert[property];
+            });
+            console.log(item);
+            newHTML += template.split(match[0]).join(insert);
+        });
+
+        console.log(newHTML);
+        loop.innerHTML = newHTML;
+        return loop;
     }
 }
 
